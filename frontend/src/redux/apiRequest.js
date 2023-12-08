58d7eb7e9b58244d4fb4e4779431ae1d34574d66
Duy2401 <longduy2410@gmail.com>
2023-12-08 13:45:57 +0700

    upload

--- frontend/src/redux/apiRequest.js
+++ frontend/src/redux/apiRequest.js
@@ -99,7 +99,7 @@ import {
 export const loginUser = async (user, dispatch, navigate) => {
   dispatch(loginStart());
   try {
-    const res = await axios.post("qlda-api.vercel.app/v1/auth/login", user);
+    const res = await axios.post("/v1/auth/login", user);
     dispatch(loginSuccess(res.data));
     navigate("/");
   } catch (err) {
